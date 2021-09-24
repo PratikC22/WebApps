1dc1f6cec2f288b774465505b23b33301528b102
Pratik Chaudhari <84624268+PratikC22@users.noreply.github.com>
2021-09-24 18:15:28 +0530

    Update Main.js

--- ES6 module/Main.js
+++ ES6 module/Main.js
@@ -9,4 +9,4 @@ let sortedArray = [1, 2, 3, 4, 5, 6];
 console.log(bubbleSort(array));
 console.log(selectionSort(array));
 console.log(mergeSort(merge(array1, array2)));
-console.log("Entry found at index " + binarySearch(sortedArray, 4));
+console.log(binarySearch(sortedArray, 4));
