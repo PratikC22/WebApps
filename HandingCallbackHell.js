409dfcd0f88285ee9d5e662fbd36cb116a4a52b0
Pratik Chaudhari <84624268+PratikC22@users.noreply.github.com>
2021-09-24 12:42:55 +0530

    Update HandingCallbackHell.js

--- HandingCallbackHell.js
+++ HandingCallbackHell.js
@@ -1,9 +1,9 @@
 //Handling callback hell using promise 
-var promise = new Promise(function (resolve, reject) {
+var p = new Promise(function (resolve, reject) {
     resolve(2);
 });
 
-promise.then(addition).then(Subtract).then(multiplication).then((msg) => {
+p.then(addition).then(subtract).then(multiplication).then((msg) => {
     console.log('output: ' + msg);
 }).catch((err) => {
     console.log(err);
@@ -13,7 +13,7 @@ function addition(val) {
     return (val + 5);
 }
 
-function Subtract(val) {
+function subtract(val) {
     return (val - 3);
 }
 
