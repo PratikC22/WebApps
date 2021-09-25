1884980f2551fe98bf3d11f36731ac69684262c2
Pratik Chaudhari <84624268+PratikC22@users.noreply.github.com>
2021-09-25 07:49:25 +0530

    Update Main.js

--- ES6 module/Sorting algorithms/Main.js
+++ ES6 module/Sorting algorithms/Main.js
@@ -1,12 +1,10 @@
 
-import { bubbleSort, mergeSort, merge, selectionSort, binarySearch } from './Utility.js';
+import { bubbleSort, mergeSort, selectionSort, binarySearch } from './Utility.js';
 
 let array = [4, 2, 11, 9, 45, 3];
-let array1 = [3, 99, 2, 34, 43];
-let array2 = [1, 6, 55, 44, 65];
 let sortedArray = [1, 2, 3, 4, 5, 6];
 
 console.log(bubbleSort(array));
 console.log(selectionSort(array));
-console.log(mergeSort(merge(array1, array2)));
+console.log(mergeSort(array));
 console.log(binarySearch(sortedArray, 4));
